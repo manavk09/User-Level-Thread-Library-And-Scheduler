07949ebfe25ce853abb7c350cadc198364059dbe
kpatel.24@outlook.com <kpatel.24@outlook.com>
2023-03-16 21:34:55 -0400

    Test

--- benchmarks/test.c
+++ benchmarks/test.c
@@ -11,16 +11,26 @@
  * This will not be graded.
  */
 
+pthread_mutex_t mutex;
+
 int foo(){
 	printf("foo\n");
 	int i = 50;
+	pthread_mutex_lock(&mutex);
+	printf("foo locked\n");
+	worker_yield();
+	pthread_mutex_unlock(&mutex);
+	printf("foo unlocked\n");
 	pthread_exit(&i);
 }
 
 int bar(){
 	printf("bar\n");
-	worker_yield();
 	int i = 60;
+	pthread_mutex_lock(&mutex);
+	printf("bar locked\n");
+	pthread_mutex_unlock(&mutex);
+	printf("bar unlocked\n");
 	pthread_exit(&i);
 }
 
@@ -31,13 +41,15 @@ int main(int argc, char **argv) {
 	pthread_t barThread;
 	int* fooValue = malloc(sizeof(int));
 	int* barValue = malloc(sizeof(int));
-	
+	pthread_mutex_init(&mutex, NULL);
+
 	pthread_create(&fooThread, NULL, &foo, 0);
 	pthread_create(&barThread, NULL, &bar, 0);
 	
 	//printf("In main, going to join.\n");
 
 	pthread_join(fooThread, (void *) &fooValue);
+	printf("Foo join done in main\n");
 	pthread_join(barThread, (void *) &barValue);
 	printf("Foo value: %d, Bar value: %d\n", *fooValue, *barValue);
 
